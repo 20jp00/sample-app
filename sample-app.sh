dc311fb9c983b668c182815562d968743f028cb4
Jon Poli <20jp00@gmail.com>
2024-02-25 19:44:40 +0000

    Changed port from 8080 to 5050.

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+# Check if the tempdir exists, if not, create it along with its subdirectories
+mkdir -p tempdir
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
+# Copy the necessary files into the tempdir
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+# Create or overwrite the Dockerfile in tempdir
+cat <<EOF > tempdir/Dockerfile
+FROM python
+RUN pip install --upgrade pip
+RUN pip install flask
+COPY ./static /home/myapp/static/
+COPY ./templates /home/myapp/templates/
+COPY sample_app.py /home/myapp/
+EXPOSE 5050
+CMD ["python", "/home/myapp/sample_app.py"]
+EOF
 
+# Navigate into tempdir and build the Docker image
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-docker ps -a 
+
+# Run the Docker container
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+
+# List the running containers
+docker ps -a
